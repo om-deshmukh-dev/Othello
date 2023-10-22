f2e7f7d7b9390750a0c695e10836998dd2432a4a
Om Deshmukh <od66@cornell.edu>
2023-10-22 18:47:21 -0400

    Implemented bug fixes in piece.ml and position.ml

--- lib/piece.mli
+++ lib/piece.mli
@@ -1,30 +1,20 @@
-open Map
+(* open Map open Position (* module type Positions_mapType = sig type t = int *
+   int
 
-module type Positions_mapType = sig
-  type t = int * int
+   val find_opt : t -> t -> int end *)
 
-  val find_opt : t -> t -> int
-end
+   module type LegitMoveType = sig type piece = | Black | White
 
-module type LegitMoveType = sig
-  type piece = Black | White
+   val northeast : int -> int -> (int * int) list val east : int -> int -> (int
+   * int) list val southeast : int -> int -> (int * int) list val south : int ->
+   int -> (int * int) list val southwest : int -> int -> (int * int) list val
+   west : int -> int -> (int * int) list val northwwest : int -> int -> (int *
+   int) list val north : int -> int -> (int * int) list val
+   can_black_play_helper : piece option list -> bool val can_black_play : piece
+   option list -> bool val can_white_play_helper : piece option list -> bool val
+   can_white_play : piece option list -> bool val is_legit : piece
+   Positions_map.t -> int -> int -> piece -> bool (* val is_legit :
+   Positions_map_sig.key Map.t -> int -> int -> piece -> bool *) end
 
-  val northeast : int -> int -> (int * int) list
-  val east : int -> int -> (int * int) list
-  val southeast : int -> int -> (int * int) list
-  val south : int -> int -> (int * int) list
-  val southwest : int -> int -> (int * int) list
-  val west : int -> int -> (int * int) list
-  val northwwest : int -> int -> (int * int) list
-  val north : int -> int -> (int * int) list
-  val can_black_play_helper : piece option list -> bool
-  val can_black_play : piece option list -> bool
-  val can_white_play_helper : piece option list -> bool
-  val can_white_play : piece option list -> bool
-  val is_legit : piece Positions_map.t -> int -> int -> piece -> bool
-  (*  val is_legit : Positions_map_sig.key Map.t -> int -> int -> piece -> bool
-*)
-end
-
-(*module Positions_map : Positions_mapType
-  module LegitMove : LegitMoveType*)
+   (*module Positions_map : Positions_mapType module LegitMove :
+   LegitMoveType*) *)
